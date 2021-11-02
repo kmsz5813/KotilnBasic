6f54dc2c231fdbb934c286c20d6ea323b11524a0
김민성 <kmsz5813@naver.com>
2021-11-02 11:19:33 +0900

    로그 찍어보기

--- app/src/main/java/com/nepplus/kotilnbasic/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotilnbasic/MainActivity.kt
@@ -2,10 +2,24 @@ package com.nepplus.kotilnbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLog.setOnClickListener {
+
+//    Ctrl + / : 이 줄을 주석으로 변경. / 주석 해체
+//         주석 : 코드에 메모 남기기.
+
+//            로그를 찍어보기
+            Log.d("메인화면", "로그찍기 버튼이 눌림")
+            Log.e("메인화면","Log.e로 찍어보기")
+
+        }
+
     }
 }
